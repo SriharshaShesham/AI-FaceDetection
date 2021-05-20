5c90807b8abd2e3df9877441ee07a6c5b09188e5
SriharshaShesham <sriharsha@sheshams.in>
2021-05-20 14:59:00 -0400

    Face Detection-Using MTCNN

--- public/js/script.js
+++ public/js/script.js
@@ -6,35 +6,56 @@ Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
-  ]).then(start)
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.mtcnn.loadFromUri('/models')
+]).then(start)
 
-  function start() {
-      document.body.append('Models Loaded')
-      
-      navigator.getUserMedia(
-          { video:{} },
-          stream => video.srcObject = stream,
-          err => console.error(err)
-      )
-      
-  }
+function start() {
+    document.body.append('Models Loaded')
+
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+
+}
+
+
+const mtcnnForwardParams = {
+    // number of scaled versions of the input image passed through the CNN
+    // of the first stage, lower numbers will result in lower inference time,
+    // but will also be less accurate
+    maxNumScales: 10,
+    // scale factor used to calculate the scale steps of the image
+    // pyramid used in stage 1
+    scaleFactor: 0.709,
+    // the score threshold values used to filter the bounding
+    // boxes of stage 1, 2 and 3
+    scoreThresholds: [0.6, 0.7, 0.7],
+    // mininum face size to expect, the higher the faster processing will be,
+    // but smaller faces won't be detected
+    minFaceSize: 200
+}
+
 
 video.addEventListener('play', () => {
-  //create the canvas from video element as we have created above
-  const canvas = faceapi.createCanvasFromMedia(video);
-  //append canvas to body or the dom element where you want to append it
-  document.body.append(canvas)
-  // displaySize will help us to match the dimension with video screen and accordingly it will draw our detections
-  // on the streaming video screen
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+    //create the canvas from video element as we have created above
+    const canvas = faceapi.createCanvasFromMedia(video);
+    //append canvas to body or the dom element where you want to append it
+    document.body.append(canvas)
+    // displaySize will help us to match the dimension with video screen and accordingly it will draw our detections
+    // on the streaming video screen
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+
+
+        const detections = await faceapi.mtcnn(video, mtcnnForwardParams)
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    }, 100)
 })
